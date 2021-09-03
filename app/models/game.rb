30cfb2e059b43d01505127954feda357130d1db8
samneter <samneter@gmail.com>
2021-09-03 07:19:41 +0100

    Switching date and time select to date_field and time_field so it saves to db

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ApplicationRecord
   belongs_to :home_club, class_name:"Club"
   belongs_to :away_club, class_name:"Club"
   belongs_to :competition
-  has_one :stream
-  has_one :live_stat
+  has_one :stream, :dependent => :destroy
+  has_one :live_stat, :dependent => :destroy
 end
--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,5 @@
 class Stream < ApplicationRecord
-  belongs_to :game
+  belongs_to :game, :dependent => :destroy
   before_validation on: :create do
     create_game
   end
