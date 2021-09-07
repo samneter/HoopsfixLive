0d3e41b401c17a6fbbbe62a6dcafac2543dbc916
samneter <samneter@gmail.com>
2021-09-07 05:54:51 +0100

    Create method to display streams by date on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @games = Game.where('date >= ?', Date.today).order(:date)
+    @streams = Stream.where('date >= ?', Date.today).order(:date)
   end
 end
