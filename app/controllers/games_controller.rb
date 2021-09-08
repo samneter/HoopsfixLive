a8eb2238eb2f62263a5f762badf32e8f27a5f16e
samneter <samneter@gmail.com>
2021-09-08 17:22:14 +0100

    Organise streams by future days on homepage

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
 
   # GET /games or /games.json
   def index
-    @games = Game.all
+      @games = Game.all
   end
 
   # GET /games/1 or /games/1.json
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @games = Game.where('date >= ?', Date.today).order(:date)
+    @games = Game.where('date >= ?', Date.current).order(:date)
     @streams = Stream.where('date >= ?', Date.today).order(:date)
+    @grouped_games = Game.where('date >= ?', Date.today).order(:date).group_by{ |g| g.date.strftime("%A %dth %B %Y")}
   end
 end
