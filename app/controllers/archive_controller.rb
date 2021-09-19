3aef2eea9cace6320f7951c0f9aa12fec163d66f
samneter <samneter@gmail.com>
2021-09-19 07:07:42 +0100

    Add archive showing only streams from the past

---
+++ app/controllers/archive_controller.rb
@@ -0,0 +1,7 @@
+class ArchiveController < ApplicationController
+  def index
+      @games = Game.where('date >= ?', Date.current).order(:date)
+      @streams = Stream.where('date >= ?', Date.today).order(:date)
+      @grouped_games = Game.where('date < ?', Date.today).order(:date).group_by{ |g| g.date.strftime("%A %d %B %Y")}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :streams
   root :to => "home#index"
   get "submit", to: "pages#submit"
+  get "archive", to: "archive#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
