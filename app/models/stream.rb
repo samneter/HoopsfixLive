8da17e447a72e8824768a750e07ad55a2abd3939
samneter <samneter@gmail.com>
2021-09-27 05:32:44 +0100

    Save live_stats_url to live_stats table on stream creation

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,6 @@
 class Stream < ApplicationRecord
-  belongs_to :game, :dependent => :destroy
+  belongs_to :game, dependent: :destroy
+  accepts_nested_attributes_for :game
   before_validation on: :create do
     create_game
   end
