d2b011b40f062baa754015cbad66fd35925fd196
samneter <samneter@gmail.com>
2021-12-21 06:37:20 +0000

    Club functions edit destroy only appear for signed in user, add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def submit
   end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   root :to => "home#index"
   get "submit", to: "pages#submit"
   get "archive", to: "archive#index"
+  get "about", to: "pages#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
