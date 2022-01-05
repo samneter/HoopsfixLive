a093b1d1172a3b06418e9aa1e9d811da82fc8485
samneter <samneter@gmail.com>
2022-01-05 05:51:23 +0000

    Add tables for past games to clubs & competitions

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -2,6 +2,6 @@ class ArchiveController < ApplicationController
   def index
       @games = Game.where('date >= ?', Date.current).order(:date)
       @grouped_games = Game.where('date < ?', Date.today).order(date: :desc).group_by{ |g| g.date.strftime("%A %-d %B %Y")}
-      @past_games = Game.where('date < ?', Date.current).order(:date)
+      @past_games = Game.where('date < ?', Date.current).order(date: :desc)
   end
 end
