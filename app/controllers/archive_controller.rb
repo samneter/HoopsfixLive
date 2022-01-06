bda238c325153f947dc27586acad0164cb274889
samneter <samneter@gmail.com>
2022-01-06 07:08:00 +0000

    Add pagination to archive

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -1,7 +1,5 @@
 class ArchiveController < ApplicationController
   def index
-      @games = Game.where('date >= ?', Date.current).order(:date)
-      @grouped_games = Game.where('date < ?', Date.today).order(date: :desc).group_by{ |g| g.date.strftime("%A %-d %B %Y")}
-      @past_games = Game.where('date < ?', Date.current).order(date: :desc)
+      @past_games = Game.where('date < ?', Date.current).order(date: :desc).paginate(page: params[:page], per_page:20)
   end
 end
