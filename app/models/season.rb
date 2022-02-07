e302c52f674c0a731fa9c6f738d882128f08af38
samneter <samneter@gmail.com>
2022-02-07 05:53:14 +0000

    Add organisation radio button to competitions form

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,6 +1,6 @@
 class Season < ApplicationRecord
   has_many :season_records
-  has_and_belongs_to_many :competitions, through: :season_records
+  has_many :competitions, through: :season_records
   has_many :teams, through: :competitions
   has_many :games
 end
