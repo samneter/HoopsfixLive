0f936461297814abc1fd192ac206ce2cfb08985e
samneter <samneter@gmail.com>
2022-03-03 06:07:25 +0000

    Made team take on club details if team details dont exist, removed display name helper, put live now on club and team show page

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,6 +1,2 @@
 module GamesHelper
-  def show_display_name(team)
-    return team.display_name if team.display_name?
-    team.club.name
-  end
 end
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,26 @@
 module TeamsHelper
+  def show_club_website(team)
+    return team.website if team.website?
+    team.club.website
+  end
+  def show_club_youtube(team)
+    return team.youtube if team.youtube?
+    team.club.youtube
+  end
+  def show_club_facebook(team)
+    return team.facebook if team.facebook?
+    team.club.facebook
+  end
+  def show_club_twitter(team)
+    return team.twitter if team.twitter?
+    team.club.twitter
+  end
+  def show_club_instagram(team)
+    return team.instagram if team.instagram?
+    team.club.instagram
+  end
+  def show_club_tiktok(team)
+    return team.tiktok if team.tiktok?
+    team.club.tiktok
+  end
 end
