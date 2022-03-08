6039fdc4c391508e1dc3aebe5d90de4ed56714ce
Christopher Morgan <63787904+ChrisGuy@users.noreply.github.com>
2022-03-08 19:02:10 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,26 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'hoopsfixlive@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'hoopsfixlive@gmail.com',
+    password:             'hoopsfixlive2021',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
 end
