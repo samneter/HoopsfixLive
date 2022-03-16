8ddf5c01be1e5571b47332e032cc1b32d1dd79ba
samneter <samneter@gmail.com>
2022-03-16 10:40:54 +0000

    Change error routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,9 @@ Rails.application.routes.draw do
   get "archive", to: "archive#index"
   get "about", to: "pages#about"
   put "approve_game/:id", to: "approve_game#update"
-  get "/404", to: "errors#not_found"
-  get "/422", to: "errors#unacceptable"
-  get "/500", to: "errors#internal_server_error"
+  get "/404", to: "errors#404"
+  get "/422", to: "errors#422"
+  get "/500", to: "errors#500"
   # match "/old_path_to_posts/:id", to: redirect("/posts/%{id}s")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
