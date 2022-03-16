25c1e2503b7072a00e1c6cecd8fa80a4893a3771
samneter <samneter@gmail.com>
2022-03-16 09:32:33 +0000

    Revert hash rocket on default mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host => 'hoopsfix-live.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'hoopsfix-live.herokuapp.com' }
 
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
