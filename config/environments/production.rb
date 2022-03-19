669060c5e891059ee2a7af4b750428180ec44ede
samneter <samneter@gmail.com>
2022-03-19 15:52:13 +0000

    Turn off mailer perform deliveries in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,7 +126,7 @@ Rails.application.configure do
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i'
   # }
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'hoopsfixlive@gmail.com'}
 
