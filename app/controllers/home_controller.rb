5e486892506b5cb0e4a6d8fe67ec3cb07f33524e
samneter <samneter@gmail.com>
2022-03-23 06:17:31 +0000

    Style stream counter, create stream counter digit methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,5 +13,10 @@ class HomeController < ApplicationController
     @page_title = 'British Basketball Live Streams'
     @page_description = 'Directory of British basketball live streams'
     @page_keywords = 'basketball live streams, bbl live streams, nbl live'
+    @counter_1st_number = Game.count.to_s.rjust(5, "0").split('').first
+    @counter_2nd_number = Game.count.to_s.rjust(5, "0").split('').second
+    @counter_3rd_number = Game.count.to_s.rjust(5, "0").split('').third
+    @counter_4th_number = Game.count.to_s.rjust(5, "0").split('').fourth
+    @counter_5th_number = Game.count.to_s.rjust(5, "0").split('').fifth
   end
 end
