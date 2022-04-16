4db81367f69d97d230c83a54cd19626131d02b25
samneter <samneter@gmail.com>
2022-04-16 10:15:23 +0100

    Add forward slash to localhost on cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://live.hoopsfix.com, http://localhost:3000'
+    origins 'https://live.hoopsfix.com, http://localhost:3000/'
     resource '*', headers: :any, methods: [:get, :post]
   end
 end
